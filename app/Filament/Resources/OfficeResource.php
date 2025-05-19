24a7ff41aa9f49b73dd98e792d3778edef452a76
asus <zunedisinambela@gmail.com>
2025-05-19 20:47:00 +0700

    [refactor] change icon

--- app/Filament/Resources/OfficeResource.php
+++ app/Filament/Resources/OfficeResource.php
@@ -17,7 +17,7 @@ class OfficeResource extends Resource
 {
     protected static ?string $model = Office::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/ScheduleResource.php
+++ app/Filament/Resources/ScheduleResource.php
@@ -17,7 +17,7 @@ class ScheduleResource extends Resource
 {
     protected static ?string $model = Schedule::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clock';
 
     public static function form(Form $form): Form
     {
